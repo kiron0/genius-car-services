9107d4d4af2bd4275307d1784557ae6a5095575f
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-04-22 21:02:48 +0600

    fixed more bugs

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import PageTitle from "../../Shared/PageTitle/PageTitle";
 
 const Checkout = () => {
-    return (
-        <div>
-            <h2>Please Checkout your booking</h2>
-        </div>
-    );
+  return (
+    <div>
+      <PageTitle title="Checkout"></PageTitle>
+      <h2>Please Checkout your booking</h2>
+    </div>
+  );
 };
 
 export default Checkout;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,44 +1,60 @@
-import { signOut } from 'firebase/auth';
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link } from 'react-router-dom';
-import auth from '../../../firebase.init';
-import logo from '../../../images/logo.png'
+import { signOut } from "firebase/auth";
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
+import logo from "../../../images/logo.png";
 
 const Header = () => {
-    const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth);
 
-    const handleSignOut = () =>{
-        signOut(auth);
-    }
+  const handleSignOut = () => {
+    signOut(auth);
+  };
 
-    return (
-        <>
-            <Navbar collapseOnSelect expand="lg" sticky='top' bg="primary" variant="dark">
-                <Container>
-                    <Navbar.Brand as={Link} to="/">
-                        <img height={30} src={logo} alt="" />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-                        <Nav className="ms-auto">
-                            <Nav.Link href="home#services">Services</Nav.Link>
-                            <Nav.Link href="home#experts">Experts</Nav.Link>
-                            <Nav.Link as={Link} to="about">About</Nav.Link>
-                            {
-                                user ?
-                                    <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>Log Out</button>
-                                :
-                                <Nav.Link as={Link} to="login">
-                                Login
-                            </Nav.Link>}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
-    );
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        sticky="top"
+        bg="primary"
+        variant="dark" className="py-3"
+      >
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img height={30} src={logo} alt="" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            id="responsive-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav className="ms-auto">
+              <Nav.Link href="/">Services</Nav.Link>
+              <Nav.Link href="/">Experts</Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              {user ? (
+                <button
+                  className="btn btn-link text-white text-decoration-none"
+                  onClick={handleSignOut}
+                >
+                  Log Out
+                </button>
+              ) : (
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
 export default Header;
